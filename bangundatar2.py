804f7d4f4a5d11c737ff10f3f99fb2c99a14e0d4
Hasanul Bashori <145761713+ocanhb@users.noreply.github.com>
2024-05-06 11:25:16 +0700

    bangundatar2.py

--- bangundatar2.py
+++ bangundatar2.py
@@ -7,18 +7,14 @@ Created on Mon May  6 10:58:22 2024
 
 from bangundatar import Persegi, Lingkaran, Segitiga, PersegiPanjang, Trapesium
 
-def main():
-    persegi = Persegi(4)
-    lingkaran = Lingkaran(7)
-    segitiga = Segitiga(6, 4)
-    persegi_panjang = PersegiPanjang(8, 3)
-    trapesium = Trapesium(6, 8, 5)
+persegi = Persegi(4)
+lingkaran = Lingkaran(7)
+segitiga = Segitiga(6, 4)
+persegi_panjang = PersegiPanjang(8, 3)
+trapesium = Trapesium(6, 8, 5)
 
-    print("Luas Persegi:", persegi.hitung_luas())
-    print("Luas Lingkaran:", lingkaran.hitung_luas())
-    print("Luas Segitiga:", segitiga.hitung_luas())
-    print("Luas Persegi Panjang:", persegi_panjang.hitung_luas())
-    print("Luas Trapesium:", trapesium.hitung_luas())
-
-if __name__ == "__main__":
-    main()
+print("Luas Persegi:", persegi.hitung_luas())
+print("Luas Lingkaran:", lingkaran.hitung_luas())
+print("Luas Segitiga:", segitiga.hitung_luas())
+print("Luas Persegi Panjang:", persegi_panjang.hitung_luas())
+print("Luas Trapesium:", trapesium.hitung_luas())
